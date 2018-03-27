aa719f35e1c6998bf6f2277eaa95da665735e5af
BIT Student <classroom@BGIT.rs>
2018-03-27 13:01:21 +0200

    Book-0 App navigation

--- src/app/App.js
+++ src/app/App.js
@@ -1,9 +1,22 @@
 import React from 'react'
 import 'materialize-css/dist/css/materialize.css'
+import { Switch, Route } from 'react-router-dom' 
+
+import { Header } from './Components/Header/Header'
+import { Footer } from './Components/Footer/Footer'
+import { FeedPage } from './Containers/FeedPage/FeedPage'
 
 export const App = () => {
 	return (
-		<h1>Basic structure</h1>
+		<div>
+			<Header />
+			<Switch>
+				<Route exact path="/" component={FeedPage}/> 
+				<Route exact path="/people"/> 
+				<Route exact path="/profile"/> 
+			</Switch>
+			<Footer />
+		</div>
 	)
 }
 
